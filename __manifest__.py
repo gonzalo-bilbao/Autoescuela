c6ead3408b2d562dbb6c8e5ab05f3fa42c702edc
Gonzalo Bilbao <gondiferoca@gmail.com>
2026-01-16 18:49:54 +0000

    Finalización del modulo

--- __manifest__.py
+++ __manifest__.py
@@ -12,31 +12,31 @@ trabajamos con github para crear un modulo en odoo mediante visual studio code
     'author': "Alberto Luque, Vicente Mena, Gonzalo Bilbao",
     'website': "https://github.com/gonzalo-bilbao/Autoescuela",
 
-    # Categories can be used to filter modules in modules listing
-    'category': 'Administration', # He cambiado Uncategorized por algo más estándar
+    'category': 'Administration',
     'version': '1.0',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
     'data': [
         'security/autoescuela_security.xml',
         'security/ir.model.access.csv',
-        
-        # 'data/sequence_data.xml',        
-        #'views/views.xml',
-        #'views/templates.xml',
 
-        'views/autoescuela_menus.xml', 
+        'data/autoescuela_sequence.xml',
+        
         'views/autoescuela_alumno_view.xml',
         'views/autoescuela_profesor_view.xml',
+        'views/autoescuela_examen_view.xml', 
+        'views/autoescuela_menus.xml', 
+
+        'reports/autoescuela_alumno_report.xml',
+        'reports/autoescuela_autoescuela_report.xml',
+        'reports/autoescuela_examen_report.xml',
+        'reports/autoescuela_profesor_report.xml',
     ],
-    # only loaded in demonstration mode
+    
     'demo': [
         'demo/demo.xml',
     ],
-    # indicamos que es una aplicacion para que salga en el menú principal
     'application': True,
     'installable': True,
 }
